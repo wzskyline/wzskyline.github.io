f8a2c6146bbfdb19094f6bcd6c22d0900acf2bed
unknown <wzskyline@qq.com>
2020-04-05 14:31:17 +0800

    0405

--- README.md
+++ README.md
@@ -1,20 +1,4 @@
-# wzskyline
-var obj = AV.Object.extend('file');
-var obj = new obj();  
-obj.set('state', 0);
-obj.save().then(function (obj) { 
-    console.log('New object created with objectId: ' + obj.id); 
-}, function (error) { 
-    console.error('Failed to create new object, with error message: ' + error.message);
-});
-# wzskyline
+# 学习记录
 
-  
-const query = new AV.Query('file').descending('createdAt').equalTo('state', 1);
-const size = 1;
-query.limit(size); 
-query.skip(this.data.page * size); 
-query.find().then(function (list) {
-    item = list[0]
-    console.log(item.id,item.attributes.name)
-})
+ 
+ 
---
+++ src/js/my.js
@@ -0,0 +1,28 @@
+ 
+ui={
+  menu:function(path){
+	  html = `
+	  <li><a><i class="fa fa-home"></i> wzskyline <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="${path}/wzskyline/index.html">wzskyline</a></li>
+                      <li><a href="${path}/wzskyline/nodejs.html">nodejs</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-edit"></i> py <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                    <li><a href="${path}/py/index.html">py</a></li>
+                    <li><a href="${path}/py/py.html">nodejs</a></li>
+                    </ul>
+                  </li>
+	  `;
+	  $("#wzskyline").html(html)
+  },
+  head:function(){
+	   html = `
+	   
+	  `;
+	  $("#pageHeader").html(html)
+	   
+  },
+} 
+ 
