7184e216e765452c10f7dc32212c84647972dc0b
easyatgitee <wzskyline@qq.com>
2020-04-13 06:33:42 +0800

    0413

--- src/js/my.js
+++ src/js/my.js
@@ -7,6 +7,9 @@ ui={
                       <li><a href="${path}/wzskyline/index.html">wzskyline</a></li>
                       <li><a href="${path}/wzskyline/nodejs.html">nodejs</a></li>
                       <li><a href="${path}/wzskyline/git.html">git</a></li>
+                      <li><a href="${path}/wzskyline/dos.html">dos</a></li>
+                      <li><a href="${path}/wzskyline/linux.html">linux</a></li>
+                      <li><a href="${path}/wzskyline/tree.html">tree</a></li>
                     </ul>
                   </li>
                   <li><a><i class="fa fa-edit"></i> py <span class="fa fa-chevron-down"></span></a>
